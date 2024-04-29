9d77eb8b691119b496f53ddb008347ba2e49dcd7
William Elliman <william.elliman@gmail.com>
2024-04-28 20:48:07 -0500

    Removed debug info

--- GaugeCluster.cpp
+++ GaugeCluster.cpp
@@ -1,11 +1,7 @@
 #include "GaugeCluster.h"
-#include <iostream>
 
 GaugeCluster::GaugeCluster(){
-
-    std::cout << "gauge count: " << gauge_names.size() << std::endl;
     for(int i = 0; i < gauge_names.size(); i++){
-        std::cout << "Creating gauge for " << gauge_names[i] << std::endl;
         Gauge *g = new Gauge(gauge_names[i]);
         g->set_expand(true);
         attach(*g,i,0);
